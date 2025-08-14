bca5377af94593cfb2f71c4ec6ae07af0375d20b
yaircharit <yaircharit@gmail.com>
2025-08-14 17:50:15 +0300

    Seperated Phermone class to a different file

---
+++ Assets/Scripts/Pheromone.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public enum PheromoneType { Food, Home, Nest, Danger } //TODO: Implement Nest & Danger pheromones logic
+
+public class Pheromone
+{
+    public PheromoneType Type;
+    public float Value;
+    public Vector3Int Position;
+
+    private Color color;
+    public Color Color => new(color.r, color.g, color.b, Value);
+
+    public Pheromone(PheromoneType type, float value, Vector3Int position)
+    {
+        Type = type;
+        Value = value;
+        Position = position;
+
+        color = type switch
+        {
+            PheromoneType.Food => new Color(1f, 0f, 0f, 0f),// Red for food
+            PheromoneType.Home => new Color(0f, 0f, 1f, 0f),// Blue for home
+            _ => Color.white,// Default color
+        };
+    }
+
+    public float Decay(float decayFactor)
+    {
+        return Value -= decayFactor;
+    }
+
+    public override string ToString()
+    {
+        return $"Pheromone Type: {Type}, Value: {Value}, Position: {Position}";
+    }
+}
